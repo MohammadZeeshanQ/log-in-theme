b2c89de1414f328394d4d4a19ab827648b6119db
Zeeshan Dell <mohammadzeshan610@yahoo.com>
2021-05-24 12:19:42 +0300

    Housekeeping

--- src/components/pages/PagePink.js
+++ src/components/pages/PagePink.js
@@ -1,12 +1,7 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography, Button, TextField, Checkbox } from '@material-ui/core';
+import { Grid, Typography, Button, TextField, Checkbox, FormControlLabel, FormControl } from '@material-ui/core';
 
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 
 import GridImage from '../../assets/images/Image1.png'
 import { CheckBox } from '@material-ui/icons';
